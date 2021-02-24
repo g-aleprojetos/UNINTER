5e57b18fb2ac8b602b4b711d7f8166bec925fcd9
g-aleprojetos <g.aleprojetos@gmail.com>
2021-02-24 10:18:00 -0300

    POO - aula01

---
+++ Tecnologia em desenvolvimento de aplicativos para dispositivos móveis/Programação Orientada a Objetos/aula01/Leonidas/src/empresa/Principal.java
@@ -0,0 +1,33 @@
+package empresa;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Digite seu palpite: ");
+		
+		int palpite = teclado.nextInt();
+		String msg;
+		
+		while(palpite!= 10000) {
+			
+			msg = palpite<1000?"Um pouco mais...":"Um pouco menos...";//operador ternário
+			System.out.println(msg);
+			/*
+			if(palpite <10000) {
+				System.out.println("Um pouco mais...");
+			}else {
+				System.out.println("Um pouco menos...");
+			}*/
+			System.out.print("Digite seu próximo palpite: ");
+			palpite = teclado.nextInt();
+		}
+		
+		System.out.println("Está correto!!!");
+		
+
+	}
+
+}
---
+++ Tecnologia em desenvolvimento de aplicativos para dispositivos móveis/Programação Orientada a Objetos/aula01/ProjetoInicial/src/empresa/Principal.java
@@ -0,0 +1,17 @@
+package empresa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int idade = 10;
+		float peso;
+		String nome = "Alexandre";
+		int tamanho = nome.length();
+		
+		System.out.println(tamanho);
+		System.out.printf("Olá Mundo!");
+		
+
+	}
+
+}
